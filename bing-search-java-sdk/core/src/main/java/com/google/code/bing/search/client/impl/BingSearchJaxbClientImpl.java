7ebb0795e65962d0331a3cde1f9c1460ca31dec2
nabeelmukhtar <nabeelmukhtar@01242c85-1573-8ad2-8154-6f9eeca4df28>
2010-03-29 12:11:15 +0000

    Initial commit of bing-search-java-sdk

--- bing-search-java-sdk/core/src/main/java/com/google/code/bing/search/client/impl/BingSearchJaxbClientImpl.java
+++ bing-search-java-sdk/core/src/main/java/com/google/code/bing/search/client/impl/BingSearchJaxbClientImpl.java
@@ -2,24 +2,13 @@
  *
  */
 package com.google.code.bing.search.client.impl;
-import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.StringWriter;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
-import javax.xml.transform.sax.SAXSource;
-
-import org.xml.sax.Attributes;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xml.sax.XMLReader;
-import org.xml.sax.helpers.XMLFilterImpl;
-import org.xml.sax.helpers.XMLReaderFactory;
 
 import com.google.code.bing.search.client.BingSearchClient;
 import com.google.code.bing.search.client.BingSearchException;
@@ -66,20 +55,15 @@ public class BingSearchJaxbClientImpl extends BaseBingSearchApiClient {
     protected <T> T unmarshallObject(Class<T> clazz, InputStream xmlContent) {
         try {
             Unmarshaller u  = getJaxbContext().createUnmarshaller();
-//            NamespaceFilter filter = new NamespaceFilter("http://schemas.microsoft.com/LiveSearch/2008/03/Search", true);
-//
-//          //Create an XMLReader to use with our filter
-//            XMLReader reader = XMLReaderFactory.createXMLReader();
-//
-//            filter.setParent(reader);
-//
-//            //Prepare the input, in this case a java.io.File (output)
-//            InputSource is = new InputSource(xmlContent);
-//
-//            //Create a SAXSource specifying the filter
-//            SAXSource source = new SAXSource(filter, is);
             
-            return (T) u.unmarshal(xmlContent);
+            com.microsoft.schemas.livesearch._2008._04.xml.element.SearchResponse response = (com.microsoft.schemas.livesearch._2008._04.xml.element.SearchResponse) u.unmarshal(xmlContent);
+    		for (com.microsoft.schemas.livesearch._2008._04.xml.web.WebResult result : response.getWeb().getResults().getWebResultList()) {
+    			System.out.println(result.getTitle());
+    			System.out.println(result.getDescription());
+    			System.out.println(result.getUrl());
+    		}
+            
+            return null;
         } catch (JAXBException e) {
             throw new BingSearchException(e);
         } catch (Exception e) {
@@ -154,77 +138,6 @@ public class BingSearchJaxbClientImpl extends BaseBingSearchApiClient {
 		}
 	}
 	
-	private static class NamespaceFilter extends XMLFilterImpl {
-
-	    private String usedNamespaceUri;
-	    private boolean addNamespace;
-
-	    //State variable
-	    private boolean addedNamespace = false;
-
-	    public NamespaceFilter(String namespaceUri,
-	            boolean addNamespace) {
-	        super();
-
-	        if (addNamespace)
-	            this.usedNamespaceUri = namespaceUri;
-	        else 
-	            this.usedNamespaceUri = "";
-	        this.addNamespace = addNamespace;
-	    }
-
-
-
-	    @Override
-	    public void startDocument() throws SAXException {
-	        super.startDocument();
-	        if (addNamespace) {
-	            startControlledPrefixMapping();
-	        }
-	    }
-
-
-
-	    @Override
-	    public void startElement(String arg0, String arg1, String arg2,
-	            Attributes arg3) throws SAXException {
-
-	        super.startElement(this.usedNamespaceUri, arg1, arg2, arg3);
-	    }
-
-	    @Override
-	    public void endElement(String arg0, String arg1, String arg2)
-	            throws SAXException {
-
-	        super.endElement(this.usedNamespaceUri, arg1, arg2);
-	    }
-
-	    @Override
-	    public void startPrefixMapping(String prefix, String url)
-	            throws SAXException {
-
-
-	        if (addNamespace) {
-	            this.startControlledPrefixMapping();
-	        } else {
-	            //Remove the namespace, i.e. don´t call startPrefixMapping for parent!
-	        }
-
-	    }
-
-	    private void startControlledPrefixMapping() throws SAXException {
-
-	        if (this.addNamespace && !this.addedNamespace) {
-	            //We should add namespace since it is set and has not yet been done.
-	            super.startPrefixMapping("", this.usedNamespaceUri);
-
-	            //Make sure we dont do it twice
-	            this.addedNamespace = true;
-	        }
-	    }
-
-	}
-	
 	public static void main(String[] args) throws Exception {
 		BingSearchClient client = new BingSearchJaxbClientImpl();
 		SearchResponse response = client.search(((BingSearchJaxbClientImpl) client).createSearchRequest());
@@ -244,35 +157,4 @@ public class BingSearchJaxbClientImpl extends BaseBingSearchApiClient {
 		builder.withWebRequestSearchOption(WebSearchOption.DISABLE_HOST_COLLAPSING).withWebRequestSearchOption(WebSearchOption.DISABLE_QUERY_ALTERATIONS);
 		return builder.getResult();
 	}
-	
-	/**
-	 * Stolen liberally from http://www.kodejava.org/examples/266.html
-	 */
-	protected static String convertStreamToString(InputStream is) {
-        /*
-         * To convert the InputStream to String we use the BufferedReader.readLine()
-         * method. We iterate until the BufferedReader return null which means
-         * there's no more data to read. Each line will appended to a StringBuilder
-         * and returned as String.
-         */
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        StringBuilder sb = new StringBuilder();
- 
-        String line = null;
-        try {
-            while ((line = reader.readLine()) != null) {
-                sb.append(line + "\n");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
- 
-        return sb.toString();
-    }
 }
