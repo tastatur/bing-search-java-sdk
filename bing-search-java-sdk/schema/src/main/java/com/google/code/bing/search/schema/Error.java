bad8c7cd7cf35eb66105954332e58cf1d5070b73
nabeelmukhtar <nabeelmukhtar@01242c85-1573-8ad2-8154-6f9eeca4df28>
2010-03-29 11:03:17 +0000

    Initial commit of bing-search-java-sdk

---
+++ bing-search-java-sdk/schema/src/main/java/com/google/code/bing/search/schema/Error.java
@@ -0,0 +1,145 @@
+package com.google.code.bing.search.schema;
+
+public interface Error {
+
+	/**
+	 * Gets the value of the code property.
+	 * 
+	 * @return
+	 *     possible object is
+	 *     {@link Long }
+	 *     
+	 */
+	public Long getCode();
+
+	/**
+	 * Sets the value of the code property.
+	 * 
+	 * @param value
+	 *     allowed object is
+	 *     {@link Long }
+	 *     
+	 */
+	public void setCode(Long value);
+
+	/**
+	 * Gets the value of the message property.
+	 * 
+	 * @return
+	 *     possible object is
+	 *     {@link String }
+	 *     
+	 */
+	public String getMessage();
+
+	/**
+	 * Sets the value of the message property.
+	 * 
+	 * @param value
+	 *     allowed object is
+	 *     {@link String }
+	 *     
+	 */
+	public void setMessage(String value);
+
+	/**
+	 * Gets the value of the parameter property.
+	 * 
+	 * @return
+	 *     possible object is
+	 *     {@link String }
+	 *     
+	 */
+	public String getParameter();
+
+	/**
+	 * Sets the value of the parameter property.
+	 * 
+	 * @param value
+	 *     allowed object is
+	 *     {@link String }
+	 *     
+	 */
+	public void setParameter(String value);
+
+	/**
+	 * Gets the value of the value property.
+	 * 
+	 * @return
+	 *     possible object is
+	 *     {@link String }
+	 *     
+	 */
+	public String getValue();
+
+	/**
+	 * Sets the value of the value property.
+	 * 
+	 * @param value
+	 *     allowed object is
+	 *     {@link String }
+	 *     
+	 */
+	public void setValue(String value);
+
+	/**
+	 * Gets the value of the helpUrl property.
+	 * 
+	 * @return
+	 *     possible object is
+	 *     {@link String }
+	 *     
+	 */
+	public String getHelpUrl();
+
+	/**
+	 * Sets the value of the helpUrl property.
+	 * 
+	 * @param value
+	 *     allowed object is
+	 *     {@link String }
+	 *     
+	 */
+	public void setHelpUrl(String value);
+
+	/**
+	 * Gets the value of the sourceType property.
+	 * 
+	 * @return
+	 *     possible object is
+	 *     {@link String }
+	 *     
+	 */
+	public String getSourceType();
+
+	/**
+	 * Sets the value of the sourceType property.
+	 * 
+	 * @param value
+	 *     allowed object is
+	 *     {@link String }
+	 *     
+	 */
+	public void setSourceType(String value);
+
+	/**
+	 * Gets the value of the sourceTypeErrorCode property.
+	 * 
+	 * @return
+	 *     possible object is
+	 *     {@link Long }
+	 *     
+	 */
+	public Long getSourceTypeErrorCode();
+
+	/**
+	 * Sets the value of the sourceTypeErrorCode property.
+	 * 
+	 * @param value
+	 *     allowed object is
+	 *     {@link Long }
+	 *     
+	 */
+	public void setSourceTypeErrorCode(Long value);
+
+}
