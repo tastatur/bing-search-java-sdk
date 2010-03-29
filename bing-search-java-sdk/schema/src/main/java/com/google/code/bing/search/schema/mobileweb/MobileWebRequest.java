5ec2bf664d76333d5d485ec319bbaf295ae83594
nabeelmukhtar <nabeelmukhtar@01242c85-1573-8ad2-8154-6f9eeca4df28>
2010-03-29 11:14:13 +0000

    Initial commit of bing-search-java-sdk

---
+++ bing-search-java-sdk/schema/src/main/java/com/google/code/bing/search/schema/mobileweb/MobileWebRequest.java
@@ -0,0 +1,65 @@
+package com.google.code.bing.search.schema.mobileweb;
+
+import java.util.List;
+
+public interface MobileWebRequest {
+
+	/**
+	 * Gets the value of the offset property.
+	 * 
+	 * @return
+	 *     possible object is
+	 *     {@link Long }
+	 *     
+	 */
+	public Long getOffset();
+
+	/**
+	 * Sets the value of the offset property.
+	 * 
+	 * @param value
+	 *     allowed object is
+	 *     {@link Long }
+	 *     
+	 */
+	public void setOffset(Long value);
+
+	/**
+	 * Gets the value of the count property.
+	 * 
+	 * @return
+	 *     possible object is
+	 *     {@link Long }
+	 *     
+	 */
+	public Long getCount();
+
+	/**
+	 * Sets the value of the count property.
+	 * 
+	 * @param value
+	 *     allowed object is
+	 *     {@link Long }
+	 *     
+	 */
+	public void setCount(Long value);
+
+	/**
+	 * Gets the value of the options property.
+	 * 
+	 * @return
+	 *     possible object is
+	 *     
+	 */
+	public List<MobileWebSearchOption> getMobileWebSearchOptionList();
+
+	/**
+	 * Sets the value of the options property.
+	 * 
+	 * @param value
+	 *     allowed object is
+	 *     
+	 */
+	public void setOptions(List<MobileWebSearchOption> value);
+
+}
