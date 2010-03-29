f7e25d7ba73ffb18f7a6bc5f2895032c08c17d51
nabeelmukhtar <nabeelmukhtar@01242c85-1573-8ad2-8154-6f9eeca4df28>
2010-03-29 14:39:35 +0000

    Initial commit of bing-search-java-sdk

---
+++ bing-search-java-sdk/schema/src/main/java/com/google/code/bing/search/schema/ads/impl/AdResultImpl.java
@@ -0,0 +1,68 @@
+
+package com.google.code.bing.search.schema.ads.impl;
+
+import java.io.Serializable;
+
+import com.google.code.bing.search.schema.ads.AdResult;
+
+public class AdResultImpl
+    implements Serializable, AdResult
+{
+
+    private final static long serialVersionUID = 2461660169443089969L;
+    protected Long rank;
+    protected String position;
+    protected String title;
+    protected String description;
+    protected String displayUrl;
+    protected String adLinkUrl;
+
+    public Long getRank() {
+        return rank;
+    }
+
+    public void setRank(Long value) {
+        this.rank = value;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String value) {
+        this.position = value;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String value) {
+        this.title = value;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String value) {
+        this.description = value;
+    }
+
+    public String getDisplayUrl() {
+        return displayUrl;
+    }
+
+    public void setDisplayUrl(String value) {
+        this.displayUrl = value;
+    }
+
+    public String getAdLinkUrl() {
+        return adLinkUrl;
+    }
+
+    public void setAdLinkUrl(String value) {
+        this.adLinkUrl = value;
+    }
+
+}
