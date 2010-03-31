716000211309a1f84c8630d9787dc00318af1c71
nabeelmukhtar <nabeelmukhtar@01242c85-1573-8ad2-8154-6f9eeca4df28>
2010-03-31 10:35:41 +0000

    Initial commit of bing-search-java-sdk

--- bing-search-java-sdk/schema/src/main/java/com/google/code/bing/search/schema/adapter/soap/mobileweb/MobileWebRequestImpl.java
+++ bing-search-java-sdk/schema/src/main/java/com/google/code/bing/search/schema/adapter/soap/mobileweb/MobileWebRequestImpl.java
@@ -4,12 +4,14 @@ package com.google.code.bing.search.schema.adapter.soap.mobileweb;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.code.bing.search.schema.adapter.Adaptable;
 import com.google.code.bing.search.schema.adapter.BaseAdapter;
 import com.google.code.bing.search.schema.mobileweb.MobileWebRequest;
 import com.google.code.bing.search.schema.mobileweb.MobileWebSearchOption;
+import com.microsoft.schemas.livesearch._2008._03.search.ArrayOfMobileWebSearchOption;
 
 public class MobileWebRequestImpl
-	extends BaseAdapter implements MobileWebRequest
+	extends BaseAdapter implements MobileWebRequest, Adaptable<MobileWebRequest, com.microsoft.schemas.livesearch._2008._03.search.MobileWebRequest>
 {
 
     /**
@@ -79,5 +81,31 @@ public class MobileWebRequestImpl
 	@Override
 	public void setOptions(List<MobileWebSearchOption> value) {
 		mobileWebSearchOptionList = value;
+	}
+
+	@Override
+	public void adaptFrom(
+			com.microsoft.schemas.livesearch._2008._03.search.MobileWebRequest adaptee) {
+		copyProperties(this, adaptee);
+		if (adaptee.getOptions() != null) {
+			for (com.microsoft.schemas.livesearch._2008._03.search.MobileWebSearchOption o : adaptee.getOptions().getMobileWebSearchOption()) {
+				MobileWebSearchOption result = MobileWebSearchOption.valueOf(o.name()) ;
+				getMobileWebSearchOptionList().add(result);
+			}
+		}
+	}
+
+	@Override
+	public com.microsoft.schemas.livesearch._2008._03.search.MobileWebRequest adaptTo() {
+		com.microsoft.schemas.livesearch._2008._03.search.MobileWebRequest adapter = new com.microsoft.schemas.livesearch._2008._03.search.MobileWebRequest();
+		copyProperties(adapter , this);
+		for (MobileWebSearchOption o : getMobileWebSearchOptionList()) {
+			if (adapter.getOptions() == null) {
+				adapter.setOptions(new ArrayOfMobileWebSearchOption());
+			}
+			adapter.getOptions().getMobileWebSearchOption().add(com.microsoft.schemas.livesearch._2008._03.search.MobileWebSearchOption.valueOf(o.name()));
+		}
+		
+		return adapter;
 	}
 }
