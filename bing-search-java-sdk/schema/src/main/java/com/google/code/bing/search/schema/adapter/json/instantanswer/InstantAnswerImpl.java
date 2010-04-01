c5a389d0bb077ab6b245c819535e7a5e1e643b42
nabeelmukhtar <nabeelmukhtar@01242c85-1573-8ad2-8154-6f9eeca4df28>
2010-04-01 07:00:30 +0000

    JSON Adapters. commit is broken...

--- bing-search-java-sdk/schema/src/main/java/com/google/code/bing/search/schema/adapter/json/instantanswer/InstantAnswerImpl.java
+++ bing-search-java-sdk/schema/src/main/java/com/google/code/bing/search/schema/adapter/json/instantanswer/InstantAnswerImpl.java
@@ -22,12 +22,13 @@ public class InstantAnswerImpl
         this.results = ((ResultsImpl) value);
     }
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public JSONObject adaptTo() {
 		JSONObject adapter = new JSONObject();
 		copyProperties(adapter  , this);
 		if (getResults() != null) {
-			adapter.setResults(((ResultsImpl) getResults()).adaptTo());
+			adapter.put("Results", ((ResultsImpl) getResults()).adaptTo());
 		}
 		return adapter;
 	}
@@ -35,9 +36,9 @@ public class InstantAnswerImpl
 	@Override
 	public void adaptFrom(JSONObject adaptee) {
 		copyProperties(this, adaptee);
-		if (adaptee.getResults() != null) {
+		if (adaptee.get("Results") != null) {
 			ResultsImpl results = new ResultsImpl();
-			results.adaptFrom(adaptee.getResults());
+			results.adaptFrom((JSONObject) adaptee.get("Results"));
 			setResults(results);
 		}
 	}
