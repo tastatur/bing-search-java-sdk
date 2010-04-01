19ae7d14244fd00cbf94db1da6fa09f9ffcf380f
nabeelmukhtar <nabeelmukhtar@01242c85-1573-8ad2-8154-6f9eeca4df28>
2010-04-01 06:25:20 +0000

    JSON Adapters. commit is broken...

--- bing-search-java-sdk/schema/src/main/java/com/google/code/bing/search/schema/adapter/BaseAdapter.java
+++ bing-search-java-sdk/schema/src/main/java/com/google/code/bing/search/schema/adapter/BaseAdapter.java
@@ -4,6 +4,7 @@
 package com.google.code.bing.search.schema.adapter;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.beanutils.BeanUtils;
@@ -31,6 +32,7 @@ public abstract class BaseAdapter implements Serializable {
 	@SuppressWarnings("unchecked")
 	protected void copyProperties(Object dest, Map src) {
 		try {
+			src = changeCaseOfKeys(src, false);
 			BeanUtils.populate(dest, src);
 		} catch (Exception e) {
 			log.error("Error while copying properties.", e);
@@ -41,9 +43,26 @@ public abstract class BaseAdapter implements Serializable {
 	protected void copyProperties(Map dest, Object src) {
 		try {
 			Map srcMap = BeanUtils.describe(src);
-			dest.putAll(srcMap);
+			dest.putAll(changeCaseOfKeys(srcMap, true));
 		} catch (Exception e) {
 			log.error("Error while copying properties.", e);
 		}		
+	}
+	
+	@SuppressWarnings("unchecked")
+	private Map changeCaseOfKeys(Map properties, boolean upper) {
+		Map retValue = new HashMap(properties.size());
+		for (String key : (Iterable<String>) properties.keySet()) {
+			Object value = properties.get(key);
+			if (upper) {
+				String newKey = Character.toUpperCase(key.charAt(0)) + key.substring(1);
+				retValue.put(newKey, value);
+			} else {
+				String newKey = Character.toLowerCase(key.charAt(0)) + key.substring(1);
+				retValue.put(newKey, value);
+			}
+		}
+		
+		return retValue;
 	}
 }
