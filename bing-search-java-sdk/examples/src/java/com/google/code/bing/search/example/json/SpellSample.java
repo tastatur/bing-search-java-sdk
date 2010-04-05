6732e12dd2228cbdd6016c7329413fe5cc949aae
nabeelmukhtar <nabeelmukhtar@01242c85-1573-8ad2-8154-6f9eeca4df28>
2010-04-05 16:29:18 +0000

    Samples

---
+++ bing-search-java-sdk/examples/src/java/com/google/code/bing/search/example/json/SpellSample.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package com.google.code.bing.search.example.json;
+
+import com.google.code.bing.search.client.BingSearchClient;
+import com.google.code.bing.search.client.BingSearchServiceClientFactory;
+import com.google.code.bing.search.client.enumeration.ApiProtocol;
+import com.google.code.bing.search.schema.SearchRequest;
+import com.google.code.bing.search.schema.SearchResponse;
+
+/**
+ * @author nmukhtar
+ *
+ */
+public class SpellSample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		BingSearchServiceClientFactory factory = BingSearchServiceClientFactory.newInstance();
+		BingSearchClient client = factory.createBingSearchClient(ApiProtocol.JSON);
+		SearchResponse response = client.search(createSearchRequest(client));
+		printResponse(response);
+	}
+
+	private static void printResponse(SearchResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static SearchRequest createSearchRequest(BingSearchClient client) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
