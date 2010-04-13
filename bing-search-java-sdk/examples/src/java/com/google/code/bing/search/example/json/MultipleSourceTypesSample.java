fb9ceb0c9ae3c0539e541e719af36be1faafa712
nabeelmukhtar <nabeelmukhtar@01242c85-1573-8ad2-8154-6f9eeca4df28>
2010-04-13 15:52:27 +0000

    Fixed structure...

--- bing-search-java-sdk/examples/src/java/com/google/code/bing/search/example/json/MultipleSourceTypesSample.java
+++ bing-search-java-sdk/examples/src/java/com/google/code/bing/search/example/json/MultipleSourceTypesSample.java
@@ -7,8 +7,13 @@ import com.google.code.bing.search.client.BingSearchClient;
 import com.google.code.bing.search.client.BingSearchServiceClientFactory;
 import com.google.code.bing.search.client.BingSearchClient.SearchRequestBuilder;
 import com.google.code.bing.search.client.enumeration.ApiProtocol;
+import com.google.code.bing.search.schema.AdultOption;
+import com.google.code.bing.search.schema.SearchOption;
 import com.google.code.bing.search.schema.SearchRequest;
 import com.google.code.bing.search.schema.SearchResponse;
+import com.google.code.bing.search.schema.SourceType;
+import com.google.code.bing.search.schema.relatedsearch.RelatedSearchResult;
+import com.google.code.bing.search.schema.web.WebResult;
 
 /**
  * @author nmukhtar
@@ -27,15 +32,31 @@ public class MultipleSourceTypesSample {
 	}
 
 	private static void printResponse(SearchResponse response) {
-		// TODO Auto-generated method stub
+		System.out.println("Bing API Version " + response.getVersion());
+		System.out.println("RelatedSearch results for " + response.getQuery().getSearchTerms());
+		for (RelatedSearchResult result : response.getRelatedSearch().getResults().getRelatedSearchResultList()) {
+			System.out.println(result.getTitle());
+			System.out.println(result.getUrl());
+		}
+		System.out.println("Web results for " + response.getQuery().getSearchTerms());
+		for (WebResult result : response.getWeb().getResults().getWebResultList()) {
+			System.out.println(result.getTitle());
+			System.out.println(result.getDescription());
+			System.out.println(result.getUrl());
+			System.out.println(result.getDateTime());
+		}
 		
 	}
 
 	private static SearchRequest createSearchRequest(BingSearchClient client) {
 		SearchRequestBuilder builder = client.newSearchRequestBuilder();
 		builder.withAppId("49EB1BB201E8950D5CEE9AC199C7ADD7CE08AA40");
+		builder.withQuery("microsoft");
+		builder.withSourceType(SourceType.WEB).withSourceType(SourceType.RELATED_SEARCH);
 		builder.withVersion("2.0");
 		builder.withMarket("en-us");
+		builder.withAdultOption(AdultOption.MODERATE);
+		builder.withSearchOption(SearchOption.ENABLE_HIGHLIGHTING);
 		return builder.getResult();
 	}
 }
