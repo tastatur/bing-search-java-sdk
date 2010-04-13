0efe089fde62f468c9ebd9cf022dc393c8240306
nabeelmukhtar <nabeelmukhtar@01242c85-1573-8ad2-8154-6f9eeca4df28>
2010-04-13 16:25:56 +0000

    Fixed structure...

--- bing-search-java-sdk/examples/src/java/com/google/code/bing/search/example/json/TranslationSample.java
+++ bing-search-java-sdk/examples/src/java/com/google/code/bing/search/example/json/TranslationSample.java
@@ -9,6 +9,8 @@ import com.google.code.bing.search.client.BingSearchClient.SearchRequestBuilder;
 import com.google.code.bing.search.client.enumeration.ApiProtocol;
 import com.google.code.bing.search.schema.SearchRequest;
 import com.google.code.bing.search.schema.SearchResponse;
+import com.google.code.bing.search.schema.SourceType;
+import com.google.code.bing.search.schema.translation.TranslationResult;
 
 /**
  * @author nmukhtar
@@ -27,15 +29,22 @@ public class TranslationSample {
 	}
 
 	private static void printResponse(SearchResponse response) {
-		// TODO Auto-generated method stub
-		
+		System.out.println("Bing API Version " + response.getVersion());
+		System.out.println("Translation results for " + response.getQuery().getSearchTerms());
+		for (TranslationResult result : response.getTranslation().getResults().getTranslationResultList()) {
+			System.out.println(result.getTranslatedTerm());
+		}
 	}
 
 	private static SearchRequest createSearchRequest(BingSearchClient client) {
 		SearchRequestBuilder builder = client.newSearchRequestBuilder();
 		builder.withAppId("49EB1BB201E8950D5CEE9AC199C7ADD7CE08AA40");
-		builder.withVersion("2.0");
-		builder.withMarket("en-us");
+		builder.withQuery("Espero");
+		builder.withSourceType(SourceType.TRANSLATION);
+		builder.withTranslationRequestSourceLanguage("es");
+		builder.withTranslationRequestTargetLanguage("en");
+		builder.withVersion("2.2");
+		
 		return builder.getResult();
 	}
 }
